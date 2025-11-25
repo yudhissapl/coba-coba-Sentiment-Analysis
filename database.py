d15986a94de505b836ed6df51d54094f67bf811a
lorensiusfico <lorensiusfico10@gmail.com>
2025-11-25 11:41:41 +0700

    Update database.py
    data db nya udah diisi

--- database.py
+++ database.py
@@ -5,11 +5,11 @@ import os
 
 load_dotenv()
 
-DB_USER = os.getenv("DB_USER")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
-DB_HOST = os.getenv("DB_HOST")
-DB_PORT = os.getenv("DB_PORT")
-DB_NAME = os.getenv("DB_NAME")
+DB_USER=root
+DB_PASSWORD=Lorensius10_
+DB_HOST=localhost
+DB_PORT=3306
+DB_NAME=mental_health_db
 
 DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
